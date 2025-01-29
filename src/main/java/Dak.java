bae79e9d0d6e076664357c44a508fd16a98cfd85
ChauuuLe <chau@LAPTOP-GRGF9N4F.>
2025-01-29 23:19:39 +0800

    Change greeting and bye, add echo

--- src/main/java/Dak.java
+++ src/main/java/Dak.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 public class Dak {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________________________")
-        System.out.println("Hello, I'm Dak\n
-                            What can I do for you?");
-        System.out.println("____________________________________________________________")
-        System.out.println("Bye. Hope to see you again soon!");
+    public static void printMessage(String message) {
+        System.out.println("  ____________________________________________________________");
+        System.out.println("  " + message);
+        System.out.println("  ____________________________________________________________");
+    }
+
+    public static void greeting() {
+        printMessage("Hello, I'm Dak\nWhat can I do for you?");
+    }
+    
+    public static void bye() {
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {  
+        greeting();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String message = sc.nextLine();
+            if (message.equals("bye")) {
+                break;
+            }
+            printMessage(message);    
+        }
+        bye();
     }
 }
