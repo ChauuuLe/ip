262eb3b17c15f09e3e270847bc06f438233d69a9
ChauuuLe <chau@LAPTOP-GRGF9N4F.>
2025-01-30 18:42:19 +0800

    Add list item

--- src/main/java/Dak.java
+++ src/main/java/Dak.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Dak {
     public static void printMessage(String message) {
         System.out.println("  ____________________________________________________________");
@@ -7,7 +9,7 @@ public class Dak {
     }
 
     public static void greeting() {
-        printMessage("Hello, I'm Dak\nWhat can I do for you?");
+        printMessage("Hello, I'm Dak\n  What can I do for you?");
     }
     
     public static void bye() {
@@ -17,12 +19,23 @@ public class Dak {
     public static void main(String[] args) {  
         greeting();
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> listItem = new ArrayList<>();
         while (true) {
             String message = sc.nextLine();
             if (message.equals("bye")) {
                 break;
-            }
-            printMessage(message);    
+            } else if (message.equals("list")) {
+                String listItemMessage = "";
+                for (int id = 1; id <= listItem.size(); id++) {
+                    listItemMessage += id + ". " + listItem.get(id - 1);
+                    if (id + 1 <= listItem.size())
+                        listItemMessage += "\n  ";
+                }
+                printMessage(listItemMessage);
+            } else {
+                listItem.add(message);
+                printMessage("added: " + message);
+            }   
         }
         bye();
     }
