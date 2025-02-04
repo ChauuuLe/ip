baf4a47960bd9b44f384bbddac35954892052efa
ChauuuLe <chau@LAPTOP-GRGF9N4F.>
2025-02-04 15:39:51 +0800

    Add save data to chatbot

--- src/main/java/exceptions/DukeException.java
+++ src/main/java/exceptions/DukeException.java
@@ -1,6 +1,13 @@
 package exceptions;
 
+/**
+ * Handle exception error specify for chatbot
+ */
 public class DukeException extends Exception {
+    /**
+     * Create Exception with error message
+     * @param message The error message
+     */
     public DukeException(String message) {
         super(message);
     }
