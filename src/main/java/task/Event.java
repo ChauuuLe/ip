1f5ec516a86328a466489c6cb65524456bf6201d
ChauuuLe <chau@LAPTOP-GRGF9N4F.>
2025-02-04 15:32:36 +0800

    Add java doc for files in tasks package

--- src/main/java/task/Event.java
+++ src/main/java/task/Event.java
@@ -1,20 +1,40 @@
 package task;
 
+/**
+ * Represents an Event task in the chatbot.
+ */
 public class Event extends Task {
-    private String from;
-    private String to;
+    protected String from;
+    protected String to;
 
+    /**
+     * Constructs an Event task with the given description, start time, and end time.
+     *
+     * @param description The task description.
+     * @param from        The start time of the event.
+     * @param to          The end time of the event.
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Returns the string representation of the Event task.
+     *
+     * @return The string representation of the Event task.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 
+    /**
+     * Converts the Event task to a formatted string for saving to a file.
+     *
+     * @return The Event task in a save-friendly format.
+     */
     @Override
     public String toDataString() {
         return "E | " + (isDone() ? "1" : "0") + " | " + getDescription() + " | " + from + " | " + to;
